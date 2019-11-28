d993b08c586b4a090fb9071feacf0d2a6429a4f6
csemiraz <mirazul2013@gmail.com>
2019-11-29 03:01:09 +0600

    relation create in user, product, review model

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -2,6 +2,8 @@
 
 namespace App\Model;
 
+use App\Model\Review;
+use App\Model\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
@@ -9,4 +11,14 @@ class Product extends Model
     protected $fillable = [
     	'user_id', 'name', 'detail', 'price', 'stock', 'discount',
     ];
+
+    public function user()
+    {
+    	return $this->belongsTo(User::class);
+    }
+
+    public function reviews()
+    {
+    	return $this->hasMany(Review::class);
+    }
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -2,6 +2,7 @@
 
 namespace App\Model;
 
+use App\Model\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
@@ -9,4 +10,9 @@ class Review extends Model
     protected $fillable = [
     	'product_id', 'customer', 'review', 'star',
     ];
+
+    public function product()
+    {
+    	return $this->belongsTo(Product::class);
+    }
 }
